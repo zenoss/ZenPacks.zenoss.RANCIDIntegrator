c5e40dfc5962cad040e8ff9b1f5d603fc729b49a
cluther <cluther@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-03-11 18:05:12 +0000

    * Fixes #1503: Add --update option to zenrancid. Reviewed by ian.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.RANCIDIntegrator@4158 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/RANCIDIntegrator/zenrancid.py
+++ ZenPacks/zenoss/RANCIDIntegrator/zenrancid.py
@@ -4,18 +4,29 @@
 # Copyright 2007 Zenoss, Inc.  All Rights Reserved.
 #
 ######################################################################
-import sys, os
+import sys
+import os
+import re
+import operator
 
 import Globals
 from Products.ZenUtils.ZenScriptBase import ZenScriptBase
 
 import logging
-log = logging.getLogger("zen.rancidUpdater")
+log = logging.getLogger("zen.rancid")
 
 class ZenRancid(ZenScriptBase):
     def __init__(self):
         ZenScriptBase.__init__(self, connect=True)
         self.rancidRoot = self.dmd.Devices.zRancidRoot
+
+
+    def buildOptions(self):
+        ZenScriptBase.buildOptions(self)
+        self.parser.add_option('--update', dest='update',
+            action='store_true', default=False,
+            help='Update router.db instead of replacing it')
+
 
     def run(self):
         group_buckets = {}
@@ -24,7 +35,7 @@ class ZenRancid(ZenScriptBase):
             if not self.validateDevice(dev): continue
 
             if not group_buckets.has_key(dev.zRancidGroup):
-                group_buckets[dev.zRancidGroup] = []
+                group_buckets[dev.zRancidGroup] = {}
 
             if dev.getPingStatus() == 0:
                 status = 'up'
@@ -34,19 +45,38 @@ class ZenRancid(ZenScriptBase):
             log.info("Added %s (%s) to %s with type: %s",
                 dev.id, dev.manageIp, dev.zRancidGroup, dev.zRancidType)
 
-            group_buckets[dev.zRancidGroup].append(
-                dev.manageIp + ":" + dev.zRancidType + ":" + status)
+            group_buckets[dev.zRancidGroup][dev.manageIp] = dict(
+                type=dev.zRancidType, status=status)
 
         for group, entries in group_buckets.items():
-            entries.sort()
-
-            rancid_db = open(os.path.join(self.rancidRoot, 'var',
-                group, 'router.db'), 'w')
-
-            for entry in entries:
-                rancid_db.write("%s\n" % (entry,))
+            filename = os.path.join(self.rancidRoot, 'var', group, 'router.db')
+            
+            # Only update existing entries. Don't remove any.
+            if self.options.update:
+                log.info("Updating existing %s", filename)
+                rancid_db = open(filename, 'r')
+                for line in rancid_db:
+                    match = re.match(r'([^:]+):([^:]+):([^:]+)', line.rstrip())
+                    if not match: continue
+                    ip, rType, status = match.groups()
+                    if entries.has_key(ip): continue
+                    entries[ip] = dict(type=rType, status=status)
+                
+                rancid_db.close()
+            else:
+                log.info("Replacing %s", filename)
+            
+            # Sort entries by IP address.
+            entryList = sorted(entries.items(), key=operator.itemgetter(0))
+            
+            # Write out the merged router.db file.
+            rancid_db = open(filename, 'w')
+            for ip, entry in entryList:
+                rancid_db.write("%s:%s:%s\n" % (
+                    ip, entry['type'], entry['status']))
 
             rancid_db.close()
+
 
     def validateDevice(self, device):
         if not hasattr(device, 'zRancidType'):
