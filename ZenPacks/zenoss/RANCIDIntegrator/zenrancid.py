b4200bf2bdcfb88465b436c14b4ec4d2b0962911
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-24 05:24:22 +0000

    [trunk/enterprise] update source headers per legal for 4.2.x and beyond using Summer's script fu; reviewed by gstreiff jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.RANCIDIntegrator@61802 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/RANCIDIntegrator/zenrancid.py
+++ ZenPacks/zenoss/RANCIDIntegrator/zenrancid.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
-######################################################################
-#
-# Copyright 2007 Zenoss, Inc.  All Rights Reserved.
-#
-######################################################################
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2007, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
+
+
 import sys
 import os
 import re
@@ -115,4 +120,3 @@ class ZenRancid(ZenScriptBase):
 if __name__ == "__main__":
     u = ZenRancid()
     u.run()
-
