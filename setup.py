0fe0dc48f45a9a697fd5dd9c54a8412db138363a
bedwards <bedwards@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-27 21:13:08 +0000

    Fixes ZEN-2725. Reviewed by Danny. Add marker egg to detect version.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.RANCIDIntegrator@62064 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.RANCIDIntegrator']
-INSTALL_REQUIRES = []
+INSTALL_REQUIRES = ['Zenoss >= 2.2']
 COMPAT_ZENOSS_VERS = '>= 2.2'
 PREV_ZENPACK_NAME = 'RANCIDIntegrator'
 # STOP_REPLACEMENTS
